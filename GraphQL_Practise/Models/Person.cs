eebfecf3b7059f03ffb11810a719eb86ce8fc0f2
shehroz665 <shehrozmalik665@gmail.com>
2024-07-03 20:05:51 +0500

    Add project files.

---
+++ GraphQL_Practise/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace GraphQL_Practise.Models
+{
+    public class Person
+    {
+        public int PersonKey { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public int? Age { get; set; }
+    }
+}
---
+++ GraphQL_Practise/Query/QueryType.cs
@@ -0,0 +1,23 @@
+﻿using GraphQL_Practise.Models;
+
+namespace GraphQL_Practise.Query
+{
+    public class QueryType
+    {
+        [UseProjection]
+        [UseSorting]
+        [UseFiltering]
+        public List<Person> getPersons()
+        {
+            return new List<Person>()
+            {
+                new Person() {PersonKey=1,Name="Ali",Description="ABC",Age=25},
+                new Person() {PersonKey=2,Name="Jawad",Description="ABC",Age=26},
+                new Person() {PersonKey=3,Name="Wahab",Description="ABC",Age=18},
+                new Person() {PersonKey=4,Name="Shehroz Ali",Description="ABC",Age=15},
+                new Person() {PersonKey=5,Name="Waqas",Description="ABC",Age=5},
+                new Person() {PersonKey=6,Name="Waqas",Description="ABC",Age=40}
+            };
+        }
+    }
+}
